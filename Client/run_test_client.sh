05b0429d5e9c7270fb4757c1685f2b0033312694
Jie Zan <jie.zan@mail.mcgill.ca>
2025-10-02 09:41:22 -0400

    fixedd run_test_client.sh

--- Client/run_test_client.sh
+++ Client/run_test_client.sh
@@ -1,3 +1,3 @@
 # Usage: ./run_client.sh [<server_hostname>
 
-java -cp ../Server/RMIInterface.jar:.:../Server/ Client.TCPTestClient $1 $2
+java -cp ../Server/RMIInterface.jar:.:../Server/ Client.TestTCPClient $1 $2
